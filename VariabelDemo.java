61b57e03bdca59f5a080056b021aeede99a59073
yuskayuss <yusk8499@gmail.com>
2025-03-14 23:08:42 +0700

    add java variable

---
+++ VariabelDemo.java
@@ -0,0 +1,20 @@
+public class VariabelDemo{
+  public static void main(String[] args){
+    int umur = 25;
+    double berat = 60.5;
+    char golonganDarah = 'o';
+    boolean mahasiswa = false;
+    String nama = "Alfian";
+
+    System.out.println("Nama: " + nama);
+    System.out.println("Umur: " + umur + " tahun");
+    System.out.println("Berat: " + berat + " kg");
+    System.out.println("Golongan Darah: " + golonganDarah);
+    System.out.println("Mahasiswa: " + mahasiswa);
+
+
+
+
+
+  }
+}
